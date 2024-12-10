aec11205caaf67902625a4f30b966b33c96930d1
abde-r <elasri9999@gmail.com>
2024-12-10 10:04:13 +0100

    fixed: k3s setup with vagrant.

--- K3s-Vagrant/k3s-master.sh
+++ K3s-Vagrant/k3s-master.sh
@@ -3,8 +3,6 @@ set -e
 
 sudo apt-get update -y
 sudo apt-get install -y curl
-
 curl -sfL https://get.k3s.io | sh -
 sudo k3s kubectl get nodes
-
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
