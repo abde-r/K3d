7d69a215c980057fae8cba669a590d71cc5f5116
abde-r <elasri9999@gmail.com>
2024-12-11 16:45:18 +0100

    in progress: connecting argocd.

--- K3d-ArgoCD/k3d-setup.sh
+++ K3d-ArgoCD/k3d-setup.sh
@@ -4,8 +4,8 @@ set -e
 sudo apt-get update -y
 sudo apt-get install -y ca-certificates curl gnupg lsb-release apt-transport-https
 
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
+# Install Docker
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -21,7 +21,36 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo docker --version
 
-k3d cluster create ArgoCD
+
+# Install k3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+echo "Creating k3d cluster 'dev'..."
+if k3d cluster list | grep -q "dev"; then
+    echo "Cluster 'dev' already exists. Deleting it..."
+    k3d cluster delete dev
+fi
+
+echo "Creating k3d cluster 'dev'..."
 k3d cluster create dev
 
+# Configure kubectl for the k3d cluster
+export KUBECONFIG=$(k3d kubeconfig write dev)
+
 kubectl get nodes
+
+# Install Argo CD in 'argocd' Namespace
+echo "Installing Argo CD..."
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# Wait for Argo CD Pods to Start
+echo "Waiting for Argo CD to be ready..."
+kubectl wait --for=condition=available --timeout=600s deployment/argocd-server -n argocd
+
+echo "Applying deployment.yaml..."
+kubectl create namespace dev
+kubectl apply -n dev -f deployment.yaml
+
+kubectl get all -n dev
+echo "Script execution completed successfully!"
